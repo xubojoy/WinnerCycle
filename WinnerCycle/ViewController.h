54372e8d2ab2307afd3d624f72e97476e985f5e7
mystyle <xubojoy1112@163.com>
2017-11-28 14:57:12 +0800

    Initial Commit

---
+++ WinnerCycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WinnerCycle
+//
+//  Created by xubojoy on 2017/11/28.
+//  Copyright © 2017年 xubojoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WinnerCycle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WinnerCycle
+//
+//  Created by xubojoy on 2017/11/28.
+//  Copyright © 2017年 xubojoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
